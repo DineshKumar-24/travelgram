05e2cb4914c12d5116a27916e9561b1b903ed190
DineshKumar-24 <dancerdinesh111@gmail.com>
2021-05-10 17:02:07 +0530

    second commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCbxMf4YI0R5qQS_VZhjzOmYw2ZiGICowI",
+    authDomain: "travelgram-f1083.firebaseapp.com",
+    projectId: "travelgram-f1083",
+    storageBucket: "travelgram-f1083.appspot.com",
+    messagingSenderId: "426446115507",
+    appId: "1:426446115507:web:29401184a53158227bd6c6"
+  }
 };
